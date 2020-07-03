216e1a091b38095f939600ce2b37927f855f4c99
Lazhar Ichir <hello@lazharichir.com>
2020-07-03 13:17:11 +0100

    all: adding a "buildall" script to build all deps

---
+++ buildall.sh
@@ -0,0 +1,49 @@
+#! /usr/bin/env bash
+
+echo " ";
+echo "starting!";
+
+which npm
+which tsc
+currentuser=`stat -f "%Su" /dev/console`
+
+cd packages	
+
+echo " ";
+echo "step – resource";
+
+cd resource/
+rm -f -r dist
+npm install
+npm run build
+cd ../
+
+echo " ";
+echo "step – slate-value";
+
+cd slate-value/
+rm -f -r dist
+npm install
+npm run build
+cd ../
+
+echo " ";
+echo "step – resource-service";
+
+cd resource-service/
+rm -f -r dist
+npm install
+npm run build
+cd ../
+
+echo " ";
+echo "step – slate-react-resource";
+
+cd slate-react-resource/
+rm -f -r dist
+npm install
+npm run build
+cd ../
+
+echo " ";
+echo "done!";
