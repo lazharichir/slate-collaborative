4c43a4114360ef9df3ec2239b8f78c56711034fc
Lazhar Ichir <hello@lazharichir.com>
2020-07-05 20:15:19 +0100

    resource: adding "ResourceRevision" type (string)

--- packages/resource/src/index.ts
+++ packages/resource/src/index.ts
@@ -1,7 +1,7 @@
 export {ClientId} from "./ClientId";
 export {VersionedClientId} from "./upcaster/RevisionedResource"
 
-export {Resource, ResourceId, ResourceRevision} from "./Resource";
+export {Resource, ResourceId, ResourceVersion, ResourceRevision} from "./Resource";
 export {resourceUpcaster} from "./upcaster/resourceUpcaster";
 export {resourceReducer} from "./reducer/resourceReducer";
 export {VersionedResource, VersionedResourceId, VersionedResourceRevision} from "./upcaster/RevisionedResource";
